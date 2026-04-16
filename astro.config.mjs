0f6df9a4f7054e4a2291539aae408369ce86a3b6
EWaste Kochi Dev <dev@ewastekochi.in>
2026-04-16 17:09:52 +0530

    Configure for Cloudflare Workers deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ewastekochi.com',
-  output: 'static',
-  trailingSlash: 'always',
+  output: 'hybrid',
+  adapter: cloudflare({
+    imageService: 'cloudflare',
+  }),
   integrations: [
     sitemap({
       changefreq: 'weekly',
